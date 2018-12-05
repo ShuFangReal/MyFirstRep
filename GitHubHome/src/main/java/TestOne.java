aa1960d073c51527735a9b32f340fba65c03a349
shufang <shufangreal@126.com>
2018-12-05 16:52:34 +0800

    myfirstgitdemo

---
+++ GitHubHome/src/main/java/TestOne.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Jerevon on 2018/12/5.
+ */
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("New Me");
+        System.out.println("New Me");
+        System.out.println("New Me");
+        System.out.println("New Me");
+        System.out.println("New Me");
+        System.out.println("New Me");
+    }
+}
