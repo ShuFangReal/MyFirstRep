68758fd4e3dacbbd4f90da1e0a5f5e2b06574589
shufang <shufangreal@126.com>
2018-12-05 16:32:44 +0800

    myfirstgitdemo

---
+++ GitHubHome/src/main/java/MyFirstDemoOfGit.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Jerevon on 2018/12/5.
+ */
+public class MyFirstDemoOfGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+    }
+}
